81c6ab7ce73356503b682b0b219d8127096178c9
Blandin Ilann <blandin.ilann@gmail.com>
2024-05-03 08:45:37 +0200

    script async

--- js/script.js
+++ js/script.js
@@ -15,19 +15,18 @@ function parseRssFeed(xml) {
     const articles = [];
     const items = xml.getElementsByTagName("item");
     for (let i = 0; i < items.length; i++) {
-      const article = {
-        titre: items[i].getElementsByTagName("title")[0].textContent,
-        description: items[i].getElementsByTagName("description")[0].textContent,
-        lien: items[i].getElementsByTagName("link")[0].textContent,
-        date: new Date(items[i].getElementsByTagName("pubDate")[0].textContent)
-      };
-      articles.push(article);
+        const article = {
+            titre: items[i].getElementsByTagName("title")[0].textContent,
+            description: items[i].getElementsByTagName("description")[0].textContent,
+            lien: items[i].getElementsByTagName("link")[0].textContent,
+            date: new Date(items[i].getElementsByTagName("pubDate")[0].textContent)
+        };
+        articles.push(article);
     }
     return articles;
 }
 
 function getRssFeed(url) {
-    let data;
     $.ajax({
         url: "FeedRss.php",
         method: "POST",
@@ -35,37 +34,56 @@ function getRssFeed(url) {
         data: {
             url: JSON.stringify(url)
         },
-        async: false,
+        async: true,
         success: function (result) {
-            data = result;
+            const rssContainer = document.getElementById("rss_container");
+            result.forEach(element => {
+                let html = "<h4 class='rss-cards-title'>" + element.title + "</h4>";
+                const articles = element.items;
+                for (let i = 0; i < articles.length; i++) {
+                    const article = articles[i];
+                    const date = convertDate(article.date.toString().substring(0, 24));
+                    html += "<div class='rss-cards'>";
+                    html += "<div class='rss-cards-title'>" + article.title + "</div>";
+                    html += "<br>";
+                    html += "<div class='rss-cards-description'>" + article.description + "</div>";
+                    html += "<br>";
+                    html += "<div class='rss-cards-date'>" + date["date"] + " " + date["hours"] + "</div>";
+                    html += "<br>";
+                    html += "<div class='rss-cards-link'>";
+                    html += "<a href='" + article.link + "' target='_blank'>Lire l'article</a>";
+                    html += "</div>";
+                    html += "</div>";
+                }
+                rssContainer.innerHTML += html;
+            });
         }
     });
-    return data;
 }
 
 
 function convertDate(dateString) {
     const date = new Date(dateString);
-  
+
     const day = date.getDate();
     const month = date.getMonth() + 1;
     const year = date.getFullYear();
     const hours = date.getHours();
     const minutes = date.getMinutes();
-  
+
     const ddMMYYYY = `${pad(day, 2)}/${pad(month, 2)}/${year}`;
     const mmHH = `${pad(minutes, 2)}:${pad(hours, 2)}`;
-  
+
     return { date: ddMMYYYY, hours: mmHH };
-  }
-  
-  function pad(number, length) {
+}
+
+function pad(number, length) {
     let str = number.toString();
     while (str.length < length) {
-      str = "0" + str;
+        str = "0" + str;
     }
     return str;
-  }
+}
 
 document.addEventListener("DOMContentLoaded", function () {
     const filePath = "../projects.json";
@@ -100,28 +118,6 @@ document.addEventListener("DOMContentLoaded", function () {
         "https://blog.jetbrains.com/phpstorm/feed/",
     ];
 
-    const result = getRssFeed(rssFeed);
-
-    const rssContainer = document.getElementById("rss_container");
-    result.forEach(element => {
-        let html = "<h4 class='rss-cards-title'>" + element.title + "</h4>";
-        const articles = element.items;
-        for (let i = 0; i < articles.length; i++) {
-            const article = articles[i];
-            const date = convertDate(article.date.toString().substring(0, 24));
-            html += "<div class='rss-cards'>";
-            html += "<div class='rss-cards-title'>" + article.title + "</div>";
-            html += "<br>";
-            html += "<div class='rss-cards-description'>" + article.description + "</div>";
-            html += "<br>";
-            html += "<div class='rss-cards-date'>" + date["date"] + " " + date["hours"] + "</div>";
-            html += "<br>";
-            html += "<div class='rss-cards-link'>";
-            html += "<a href='" + article.link + "' target='_blank'>Lire l'article</a>";
-            html += "</div>";
-            html += "</div>";
-        }
-        rssContainer.innerHTML += html;
-    });
+    getRssFeed(rssFeed);
 
 });
